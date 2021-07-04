b0acbb84b1b5e925ad8fae71f5a3e3edc9ef985c
Ioannis Manthopoulos <johnmanthop@gmail.com>
2021-07-04 13:45:23 +0300

    Delete compilers directory

--- compilers/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-yacc -d parser.y
-
-flex lexer.l
-
-gcc lex.yy.c y.tab.c AST.c Symbol_Table.c -g -o comp
-
--- compilers/readme.md
+++
@@ -1,1 +0,0 @@
-Compilers project for uni. A compiler frontend for a fortran-like language
